ce5d193cddfdd1a24e270ef9f47edba9eb5477f7
Taiyo Hamamoto <taiyo.ocean5123ht@gmail.com>
2022-11-05 00:20:06 +0900

    メインコンテンツの仮完成、トレンド部分の着手（ホーム画面）

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,8 +1,8 @@
-<nav class="navbar d-flex align-content-between px-md-4 px-sm-2 pb-0">
+<nav class="navbar header d-flex align-content-between px-md-4 px-sm-2 pb-0">
 
     {{-- top --}}
     <div class="d-flex justify-content-between h-43 mx-auto">
-        <img src="{{ asset('images/main-icon.png') }}" class="main-icon">
+        <img src="{{ asset('images/mascots/space.png') }}" class="main-icon">
 
         <form action="" class="d-flex form-inline align-items-center bg-white px-2 search-form">
             <input type="search" class="form-control border-0 shadow-none w-100" placeholder="恐竜を検索">
@@ -13,9 +13,8 @@
     <div class="h-43 mx-auto">
 
         <div>
-            <button class="btn bg-theme text-white h-75" >ホーム</button>
-            <button class="btn bg-theme text-white h-75" >図鑑</button>
-            <button class="btn bg-theme text-white h-75" >トレンド</button>
+            <button class="btn bg-main text-white h-75" >ホーム</button>
+            <button class="btn bg-main text-white h-75" >図鑑</button>
         </div>
         
     </div>
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -7,62 +7,114 @@
 @section('content') 
 
 
-<main class="d-flex bg-theme-back">
+<main class="d-flex bg-base">
     
     <div class="left-content d-xl-flex d-none h-100 px-4">
         <aside>
-            <h5>今週のトレンド恐竜</h5>
+            <div class="trend-header d-flex justify-content-between align-items-end">
+                <span class="d-flex align-items-center">
+                    <img src="{{ asset("images/egg.png") }}">
+                    <h6 class="m-0">トレンド恐竜</h6>
+                </span>
+                <nav class="d-flex justify-content-end">
+                    <button class="unselected-btn border-0 position-relative d-flex align-items-start justify-content-center p-0">
+                        総合
+                    </button>
+                    <button class="unselected-btn border-0 position-relative d-flex align-items-start justify-content-center p-0">
+                        月間
+                    </button>
+                    <button class="unselected-btn border-0 position-relative d-flex align-items-start justify-content-center p-0">
+                        週間
+                    </button>
+                </nav>
+            </div>
+
             <div class="trend-aside list-group">
                 @for($i=0; $i<10; $i++)
+                <img src="{{ asset("images/dinosaur-icons/therizinosaurus.png") }}">
                     <a class="list-group-item list-group-item-action">ティラノサウルス</a>
                 @endfor
             </div>
+
         </aside>
     </div>
 
 
+
+
+
+
+
     <div class="main-content d-flex justify-content-center flex-column h-100 bg-gray-100 w-100">
         @for($i=0; $i<=10; $i++)
-            <a class="d-flex align-items-center bg-white mb-3 p-4 text-black cur-p text-decoration-none">
+            <a class="d-flex align-items-center justify-content-between position-relative bg-white mb-3 p-3 text-black cur-p text-decoration-none">
 
-                <span class="d-flex justify-content-center align-items-center rounded-4 bg-info">
-                    <img class="w-75 h-75" src="{{ asset('images/thumbnail/icons8-ancient-85 (1).png') }}">
-                </span>
-                <div class="w-75 article-right">
-                    <div class="author d-flex align-items-center">
-                        <img class="rounded-circle bg-info" src="{{ asset('images/thumbnail/icons8-kawaii-dinosaur-48.png') }}">
+                <div class="d-flex justify-content-center align-items-center rounded-4 bg-info">
+                    <img class="w-75 h-75" src="{{ asset('images/thumbnails/therizinosaurus.png') }}">
+                </div>
+
+                <div class="article-right">
+                    <div class="author d-flex align-items-center flex-wrap">
+                        <img class="rounded-circle bg-info" src="{{ asset('images/user-icons/earless-seal.png') }}">
                         <p class="my-0">風呂好き</p>
                         <p class="my-0">13日前</p>
                         <i class="fa-regular fa-heart"></i>
-                        <p class="my-0">114</p>
+                        <p class="m-0">114</p>
                     </div>
-                    <h4>HTML/CSS入門〜ゼロからのWEB制作</h4>
-                    <div class="tag d-flex">
-                        <button class="btn d-flex btn-outline-dark rounded-pill p-1">
-                            <img class="rounded-circle bg-info" src="{{ asset('images/thumbnail/icons8-kawaii-dinosaur-48.png') }}">
-                            <p class="m-0">ティラノサウルス</p>
-                        </button>
-                        <button class="btn d-flex btn-outline-dark rounded-pill p-1">
-                            <img class="rounded-circle bg-info" src="{{ asset('images/thumbnail/icons8-kawaii-dinosaur-48.png') }}">
-                            <p class="m-0">テリジノサウルス</p>
-                        </button>
-                        <button class="btn d-flex btn-outline-dark rounded-pill p-1">
-                            <img class="rounded-circle bg-info" src="{{ asset('images/thumbnail/icons8-kawaii-dinosaur-48.png') }}">
-                            <p class="m-0">Ark</p>
-                        </button>
+                    <h5>認証の転換。世界中の誰もが持っているIDがあった</h5>
+
+                    <div class="tag d-flex flex-wrap">
+                        @for($j=0; $j<7; $j++)
+                            <button class="btn d-flex align-items-center border border-secondary rounded-pill p-1">
+                                <img class="rounded-circle bg-info" src="{{ asset('images/dinosaur-icons/therizinosaurus.png') }}">
+                                <p class="m-0">パキケファロ</p>
+                            </button>
+                        @endfor
                     </div>
+
                 </div>
             </a>
         @endfor
     </div>
 
 
-    <div class="d-lg-flex d-none justify-content-center right-content px-4">
-        <aside class="position-relative bg-white rounded">
-            <img class="position-absolute" src="{{ asset('images/drawing.png') }}">
-            <div class="trivia-header d-flex flex-column justify-content-center align-items-center rounded-top bg-theme text-white">
-                <h5 class="m-0">恐竜</h5>
-                <h5 class="m-0">豆知識アイコン</h5>
+
+
+
+    <div class="d-lg-inline-block d-none justify-content-center right-content px-4">
+        <aside class="mini-content mb-5">
+            <img src="{{ asset('images/mascots/drawing.png') }}">
+            <div>
+                <h5>恐竜</h5>
+                <h5>豆知識</h5>
+            </div>
+
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus iusto aspernatur sint quaerat, nobis rerum non tenetur perferendis modi vel placeat impedit maiores</p>
+            </div>
+        </aside>
+        
+
+        <aside class="mini-content mb-5">
+            <img src="{{ asset('images/mascots/photographer.png') }}">
+            <div>
+                <h5>Pick Up!</h5>
+            </div>
+
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus iusto aspernatur sint quaerat, nobis rerum non tenetur perferendis modi vel placeat impedit maiores quidem ipsam, possimus vero enim nam delectus!</p>
+            </div>
+        </aside>
+        
+
+        <aside class="mini-content">
+            <img class="end-0" src="{{ asset('images/mascots/think.png') }}">
+            <div>
+                <h5>恐竜クイズ</h5>
+            </div>
+
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus iusto aspernatur sint quaerat, nobis rerum non tenetur perferendis modi vel placeat impedit maiores quidem ipsam, possimus vero enim nam delectus!</p>
             </div>
         </aside>
     </div>
